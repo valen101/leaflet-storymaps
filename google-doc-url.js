6d7fbf1f8b6c1e3e7821273d3cb527770360aad5
valen101 <159862233+valen101@users.noreply.github.com>
2024-04-04 19:19:42 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1avCdhizXP7PIOJACsJBxnzOPHp8Tg2SGYqibqIM-t7g/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = '41680524049-lruj2l3q0leb6mskvalp36vo7i3u5578.apps.googleusercontent.com';
+var googleApiKey = 'AIzaSyCxfJ3oA5EtSUAJtj53SSzzy--Sz39B7IQ';
